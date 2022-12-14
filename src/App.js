be5d489b8671eef4dfa39f91e13184dbce3da6db
Jun Yuan <yjun@zjuici.com>
2022-12-14 11:00:08 +0800

    use materialUI

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import { useState } from "react";
-import { sculptureList } from "./data";
+import { Button } from '@mui/material';
 import './App.css';
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-
-  const totalCount = sculptureList.length;
-  const handleNextClick = () => {
-    let next = index + 1;
-    next = next < totalCount ? next : 0;
-    setIndex(next);
-  };
-
-  const handleMoreClick = () => {
-    setShowMore(!showMore);
-  };
-
-  const sculpture = sculptureList[index];
+export default function MyApp() {
   return (
-    <>
-      <button onClick={handleNextClick}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        Show details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img
-        className="avatar"
-        src={sculpture.url}
-        alt={sculpture.alt}
-      />
-    </>
+    <div>
+      <Button variant='contained'>Hello world</Button>
+    </div>
   );
 }
