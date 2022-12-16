93a5ce3a7ce219a43339968ce19c66c1fec98895
Jun Yuan <yjun@zjuici.com>
2022-12-16 17:16:14 +0800

    before flow setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
-import { Button } from '@mui/material';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
 
-export default function MyApp() {
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing')
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
+  function Canvas() {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+
+      ctx.font = "48px serif";
+      ctx.strokeText("Hello world", 10, 50);
+    });
+
+    return (
+      <canvas ref={canvasRef}></canvas>
+    );
+  }
+
   return (
-    <div>
-      <Button variant='contained'>Hello world</Button>
-    </div>
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea value={answer} onChange={handleTextareaChange} disabled={status === 'submitting'} />
+        <br />
+        <button disabled={answer.length === 0 || status === 'submitting'}>submit</button>
+      </form>
+      {error !== null && <p style={{ "color": "red" }}>{error.message}</p>}
+      <Canvas height={100} width={100} />
+    </>
   );
+}
+
+function submitForm(answer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shoudError = answer.toLowerCase() !== 'lima';
+      if (shoudError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { default as App } from "./App";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(
