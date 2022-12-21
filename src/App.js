39b1a6d116c9e7b6508666b7ebf2a40ce6fbc089
Jun Yuan <jsjyuanjun@hotmail.com>
2022-12-21 12:09:54 +0800

    chang user email

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function Accordion() {
       <Panel title='Etymology' isActive={activeIndex === 1} onShow={() => setActiveIndex(1)}>
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
       </Panel>
+      <p>Test</p>
     </>
   );
 }
