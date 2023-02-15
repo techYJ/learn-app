277cb741ba71fa89861a43cb65cca2eeeb0dd015
Jun Yuan <jsjyuanjun@hotmail.com>
2023-02-15 23:52:39 +0800

    learn css

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function Canvas() {
   });
 
   return (
-    <canvas ref={canvasRef}></canvas>
+    <canvas ref={canvasRef} style={{backgroundColor: "blue"}}></canvas>
   );
 }
 
@@ -30,7 +30,7 @@ export default function Accordion() {
   };
 
   return (
-    <>
+    <div className="global">
       <h2>Almaty, Kazakhstan</h2>
       <Panel title='About' isActive={activeIndex === 0} onShow={handleShowChange}>
         With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
@@ -39,7 +39,7 @@ export default function Accordion() {
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
       </Panel>
       <Canvas />
-    </>
+    </div>
   );
 }
 
